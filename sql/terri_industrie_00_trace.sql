521c01287f0b94206aaa63c7a08d796c32af1007
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-09 10:32:47 +0200

    Create terri_industrie_00_trace.sql

---
+++ sql/terri_industrie_00_trace.sql
@@ -0,0 +1,13 @@
+/*TERRITOIRE D'INDUSTRIE V1.0*/
+/*Creation du fichier "00_trace" qui permet de suivre l'évolution du code*/
+/* terri_industrie_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : Conseil régional Nouvelle-Aquitaine - http://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT*/
+
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2020-06-01 : TV / initialisation du code uniquement
+2020-06-01 : TV / initialisation de la structure de la base de données sur la méthanisation
