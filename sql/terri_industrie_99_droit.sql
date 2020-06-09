c764df6afef41da6fed60ad94cc93e9d3c6b9aa9
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-09 14:55:02 +0200

    add droit m_zon_terri_industrie
    Ajout des droits pour la table m_zon_terri_industrie_geo

---
+++ sql/terri_industrie_99_droit.sql
@@ -0,0 +1,14 @@
+/* TERRI_INDUSTRIE V1.0 */
+/* Creation des droits sur l'ensemble des objets */
+/* terri_industrie_99_droit.sql */
+/* PostgreSQL/PostGIS */
+/* Conseil régional Nouvelle-Aquitaine - https://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT */
+
+------------------------------------------------------------------------ 
+-- Grant : Droits des éléments
+------------------------------------------------------------------------
+
+-- Tables : met_zon.m_zon_terri_industrie_geo
+GRANT ALL ON TABLE met_zon.m_zon_terri_industrie_geo TO "pre-sig-usr";
+GRANT SELECT ON TABLE met_zon.m_zon_terri_industrie_geo TO "pre-sig-ro";
